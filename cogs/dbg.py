bf9517886ef2c82d57e66f293582b8a37d37e3ae
amelie lavender <lavenderamelie@gmail.com>
2023-11-08 20:30:57 -0500

    formatting

--- cogs/dbg.py
+++ cogs/dbg.py
@@ -1,23 +1,24 @@
 import discord
 from discord.ext import commands
 
+
 class debug(commands.Cog):
     def __init__(self, voicely):
         self.voicely = voicely
 
     @commands.command(hidden=True)
     async def ping(self, ctx):
-        await ctx.send('pong')
+        await ctx.send("pong")
 
     @commands.command(hidden=True)
     async def get_id(self, ctx, member: discord.Member):
         id = member.id
         try:
-            await ctx.send(f'{member}: {id}')
+            await ctx.send(f"{member}: {id}")
             if member == None:
                 await ctx.send(ctx.message.author.id)
         except:
-            await ctx.send('usage: :get_id `@user`')
+            await ctx.send("usage: :get_id `@user`")
 
 
 async def setup(client):
--- cogs/guild_prefs.py
+++ cogs/guild_prefs.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-import sqlite3 
+import sqlite3
 import os
 from dotenv import load_dotenv
 
@@ -9,59 +9,61 @@ load_dotenv()
 
 db = os.getenv("DATABASE")
 
+
 class settings(commands.Cog):
     def __init__(self, voicely):
         self.voicely = voicely
-    
+
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         connection = sqlite3.connect(db)
         cursor = connection.cursor()
         id = guild.id
         params = (id, 1)
-        cursor.execute('INSERT OR IGNORE INTO guilds (guild_id, x_said) VALUES (?, ?)', params) 
+        cursor.execute(
+            "INSERT OR IGNORE INTO guilds (guild_id, x_said) VALUES (?, ?)", params
+        )
         connection.commit()
         connection.close()
 
-    @commands.hybrid_command(name='set', 
-                             description
-                             ='sets the text channel that voicely will read from')
-    @app_commands.describe(channel
-                           ='choose the text channel for voicely to read from')
+    @commands.hybrid_command(
+        name="set", description="sets the text channel that voicely will read from"
+    )
+    @app_commands.describe(channel="choose the text channel for voicely to read from")
     async def set(self, ctx: commands.Context, channel: discord.TextChannel) -> None:
         connection = sqlite3.connect(db)
         cursor = connection.cursor()
-        
+
         id = ctx.guild.id
         tc = channel.id
 
-        cursor.execute('UPDATE guilds SET text_channel_id= ? WHERE guild_id= ? ', (tc, id))
-        
+        cursor.execute(
+            "UPDATE guilds SET text_channel_id= ? WHERE guild_id= ? ", (tc, id)
+        )
+
         connection.commit()
         connection.close()
-        await ctx.send(f'now watching {channel} :eyes:')
+        await ctx.send(f"now watching {channel} :eyes:")
 
-    @commands.hybrid_command(name='xsaid', 
-                             description
-                             ='enables/disables x said prefix when speaking')
-    @app_commands.describe(setting='true or false.')
-    async def xsaid(self, 
-                    ctx: commands.Context, 
-                    setting: str) -> None:
+    @commands.hybrid_command(
+        name="xsaid", description="enables/disables x said prefix when speaking"
+    )
+    @app_commands.describe(setting="true or false.")
+    async def xsaid(self, ctx: commands.Context, setting: str) -> None:
         connection = sqlite3.connect(db)
         cursor = connection.cursor()
-        
+
         id = ctx.guild.id
 
-        if setting == 'true':
-            cursor.execute('UPDATE guilds SET x_said=1 WHERE guild_id= ? ', [id])
-        if setting == 'false':
-            cursor.execute('UPDATE guilds SET x_said=0 WHERE guild_id= ? ', [id])
+        if setting == "true":
+            cursor.execute("UPDATE guilds SET x_said=1 WHERE guild_id= ? ", [id])
+        if setting == "false":
+            cursor.execute("UPDATE guilds SET x_said=0 WHERE guild_id= ? ", [id])
 
         connection.commit()
         connection.close()
-        await ctx.send(f'x said setting is now: {setting}')
-        
+        await ctx.send(f"x said setting is now: {setting}")
+
 
 async def setup(client):
     await client.add_cog(settings(client))
