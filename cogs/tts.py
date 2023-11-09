198e84e72579179072f1c66ba96c257081516580
amelie lavender <lavenderamelie@gmail.com>
2023-11-08 20:33:15 -0500

    remove unused imports

--- cogs/tts.py
+++ cogs/tts.py
@@ -1,7 +1,5 @@
-import discord
 from discord import FFmpegPCMAudio
 from discord.ext import commands
-from discord import app_commands
 from typing import Dict
 from gtts import gTTS
 import asyncio
